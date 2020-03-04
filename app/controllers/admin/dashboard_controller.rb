513cb3c0c3de5d2c6f03ad191aa236b5e0f0e961
vagrant <frederique.bordeleau@gmail.com>
2020-03-04 23:10:29 +0000

    Added db info to admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,6 +2,8 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV["HTTP_ADMIN_USERNAME"], 
                                password: ENV["HTTP_ADMIN_PASSWORD"]
   def show
+    @total_products = Product.count
+    @total_categories = Category.count
   end
 
 end
