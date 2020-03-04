caec17ff2718301b46123e18d66456503ba7fbfc
vagrant <frederique.bordeleau@gmail.com>
2020-03-04 16:04:11 +0000

    Creating new branch

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,8 @@
 Rails.configuration.stripe = {
-  :publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'],
-  :secret_key      => ENV['STRIPE_SECRET_KEY']
+  # :publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'],
+  :publishable_key => 'pk_test_UfTnwSci3ONuXw6hTLuEfyjQ00UxLRSaTm',
+  # :secret_key      => ENV['STRIPE_SECRET_KEY']
+  :secret_key      => 'sk_test_jT6irj79k1qVOOKU0FrDZgjr00WfvSsPCQ'
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
