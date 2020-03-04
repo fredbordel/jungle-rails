c457c44dde20a7d4e459c8b1e25a558573f1e54c
vagrant <frederique.bordeleau@gmail.com>
2020-03-04 22:28:17 +0000

    Created basic auth for admin pages

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,7 @@
 class Admin::DashboardController < ApplicationController
-  http_basic_authenticate_with name:"Jungle", password:"book"
+  http_basic_authenticate_with name: ENV["HTTP_ADMIN_USERNAME"], 
+                               password: ENV["HTTP_ADMIN_PASSWORD"]
   def show
   end
+
 end
