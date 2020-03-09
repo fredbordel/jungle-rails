e8863f2ea1e7e910b69c6b4776e5c0deb34f4011
vagrant <frederique.bordeleau@gmail.com>
2020-03-09 23:45:35 +0000

    Created test to validate Product form

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+    subject {
+      Product.new(name: "Name",
+                  price_cents: 25,
+                  quantity: 3,
+                  category: Category.new)
+    }
+    
+    it 'is valid with valid attributes' do
+      expect(subject).to be_valid
+    end
+
+    it 'is not valid without a name' do
+      subject.name = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'is not valid without a price' do
+      subject.price_cents = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'is not valid without a quantity' do
+      subject.quantity = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'is not valid without a category' do
+      subject.quantity = nil
+      expect(subject).to_not be_valid
+    end
+
+  end
 end
