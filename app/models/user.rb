89d46a04c3c2c944190b5a60b92e3b7769cc9494
vagrant <frederique.bordeleau@gmail.com>
2020-03-10 00:51:47 +0000

    Tested user form with specs

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,10 @@ class User < ActiveRecord::Base
 
   has_secure_password
 
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, confirmation: { case_sensitive: false }
+  validates :password, length: { minimum: 3 }
+  validates :password_confirmation, presence: true
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-
   describe 'Validations' do
-    user_subject {
+    subject {
       User.new(first_name: 'First',
                last_name: 'Last',
                email: 'something@email.com',
@@ -12,20 +11,35 @@ RSpec.describe User, type: :model do
     }
 
       it 'is valid with all valid attributes' do
-        expect(user_subject).to be_valid
+        expect(subject).to be_valid
       end
 
+      it 'is not valid without an email' do
+        subject.email = nil
+        expect(subject).to_not be_valid
+      end
+
+      it 'is not valid without a first name' do
+        subject.first_name = nil
+        expect(subject).to_not be_valid
+      end
+
+      it 'is not valid without a last name' do
+        subject.last_name = nil
+        expect(subject).to_not be_valid
+      end
+
+      it 'is not valid without a password with minimum 3 char' do
+        subject.password = nil || subject.password = 'ab'
+        expect(subject).to_not be_valid
+      end
+
+      it 'is not valid if password_confirmation != password' do
+        subject.password_confirmation = 'somethingelse'
+        expect(subject).to_not be_valid
+      end
   end
 end
 
 
-
-# it is not valid without an email
-# it is not valid without a unique email // not case sensitive
-# it is not valid without a first name
-# it is not valid without a last name
-# it is not valid without a password
-# it is not valid without a confirmation-password
-# it is not valid if password != confirmation-password
-# it is not valid if password length is < than 3
 
